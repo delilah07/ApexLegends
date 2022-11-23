457142bb1ae001550e52c36989e44453cd4abffb
Iryna Abrosimova <iryna.abrosimova@cxdisruption.com>
2022-11-23 13:38:02 +0100

    feat: add nav and click event of nav

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@ const url = "https://raddythebrand.github.io/apex-legends/data.json";
 const loader = document.querySelector("#loading");
 const loaderFrame = document.querySelector("#loading__frame");
 
+const legendsList = document.querySelector("#legends");
+const listFragment = document.createDocumentFragment();
+
+const legend = document.querySelector("#legend");
+
 async function getLegends() {
   try {
     const response = await fetch(url);
@@ -10,10 +15,40 @@ async function getLegends() {
     if (response) {
       loader.style.display = "none";
     }
+    listLegends(data);
+    viewDetail(data[0]);
   } catch (error) {
     console.log(error);
     loaderFrame.innerHTML = "Error";
   }
+}
+
+function listLegends(data) {
+  data.map(function (legend) {
+    const legendText = `
+        <img src="${legend.thumbnail.default}" alt="${legend.nickname}">
+        <div class="legend__nickname">${legend.nickname}</div>
+    `;
+    const item = document.createElement("li");
+    item.innerHTML = legendText;
+    listFragment.appendChild(item);
+
+    item.addEventListener("click", function () {
+      viewDetail(legend);
+    });
+  });
+  legendsList.appendChild(listFragment);
+}
+
+function viewDetail(data) {
+  let result = `
+        <img src="${data.thumbnail.medium}" alt="${data.nickname}">
+        <div class="legend__data">
+            <div class="data__nickname">${data.nickname}</div>
+        </div>
+  `;
+
+  legend.innerHTML = result;
 }
 
 getLegends();
